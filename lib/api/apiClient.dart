d84692d335a1d4fcfcd58019305bf90b1d9faa8d
mehedi <rakibulislammehedi4@gmail.com>
2024-11-05 10:49:57 +0600

    Taskly - API Client added!

---
+++ lib/api/apiClient.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:taskly/style/style.dart';
+
+var baseURL = "http://35.73.30.144:2005/api/v1";
+var reqHeader = {"Content-Type": "application/json"};
+
+Future<bool> LoginRequest(formValues) async {
+  Uri URL = Uri.parse("$baseURL/Login");
+  var postBody = jsonDecode(formValues);
+  var response = await http.post(URL, headers: reqHeader, body: postBody);
+  int resultCode = response.statusCode;
+  var resultBody = jsonDecode(response.body);
+  if (resultCode == 200 && resultBody['status'] == 'success') {
+    SuccessToast("Request Success");
+    return true;
+  } else {
+    ErrorToast("Request failed! try again");
+    return false;
+  }
+}
+
+Future<bool> RegistrationRequest(formValues) async {
+  Uri URL = Uri.parse("$baseURL/Registration");
+  var postBody = jsonEncode(formValues);
+  var response = await http.post(URL, headers: reqHeader, body: postBody);
+  int resultCode = response.statusCode;
+  var resultBody = jsonDecode(response.body);
+
+  if (resultCode == 200 && resultBody['status'] == "success") {
+    SuccessToast("Request success");
+    return true;
+  } else {
+    ErrorToast("Request failed! try again");
+    return false;
+  }
+}
+
+Future<bool> VerifyEmailRequest(Email) async {
+  Uri URL = Uri.parse("$baseURL/RecoverVerifyEmail");
+  var response = await http.get(URL, headers: reqHeader);
+  int resultCode = response.statusCode;
+  var resultBody = jsonDecode(response.body);
+
+  if (resultCode == 200 && resultBody['status'] == "success") {
+    SuccessToast("Request Success");
+    return true;
+  } else {
+    ErrorToast("Request failed! try again");
+    return false;
+  }
+}
+
+Future<bool> VerifyOTPRequest(Email, OTP) async {
+  return true;
+}
+
+Future<bool> SetPasswordRequest(formValues) async {
+  return true;
+}
